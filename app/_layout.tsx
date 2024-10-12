bb308ef9076112cc7b4c39d11e7b81af64af2919
Simon Van Mello <simon@ingenum.be>
2024-10-12 13:52:24 +0200

    feat(1): order imports

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,16 @@
+import { useEffect } from 'react';
+import { useFonts } from 'expo-font';
+import { Stack } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
 import {
   DarkTheme,
   DefaultTheme,
   ThemeProvider,
 } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
+
 import 'react-native-reanimated';
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { useColorScheme } from '@app/hooks/useColorScheme';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
--- components/Collapsible.tsx
+++ components/Collapsible.tsx
@@ -1,10 +1,10 @@
-import Ionicons from '@expo/vector-icons/Ionicons';
 import { PropsWithChildren, useState } from 'react';
 import { StyleSheet, TouchableOpacity, useColorScheme } from 'react-native';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
-import { Colors } from '@/constants/Colors';
+import { ThemedText } from '@app/components/ThemedText';
+import { ThemedView } from '@app/components/ThemedView';
+import { Colors } from '@app/constants/Colors';
 
 export function Collapsible({
   children,
