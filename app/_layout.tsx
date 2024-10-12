7ad5733b1f41ad4db3b1c5a30a88762081c88029
Simon Van Mello <simon@ingenum.be>
2024-10-12 15:44:03 +0200

    feat(1): config reactotron

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,15 @@
 import { Stack } from 'expo-router';
 
-export default function RootLayout() {
+if (__DEV__) {
+  require('../ReactotronConfig');
+}
+
+const Layout = () => {
   return (
     <Stack>
       <Stack.Screen name="index" />
     </Stack>
   );
-}
+};
+
+export default Layout;
