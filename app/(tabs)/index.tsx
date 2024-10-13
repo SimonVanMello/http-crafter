cf4595d1e0aa4475543f81ee8ae0e83fe39137b8
Simon Van Mello <simon@ingenum.be>
2024-10-13 10:03:54 +0200

    feat(1): use IOS colors

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,6 @@
 import { Button, ButtonText } from '@app/components/gluestack-ui/button';
 import { Heading } from '@app/components/gluestack-ui/heading';
+import { Text } from '@app/components/gluestack-ui/text';
 import PageView from '@app/components/page-view/PageView';
 import Section from '@app/components/Section';
 import useColorMode from '@app/hooks/useColorMode.hook';
@@ -11,6 +12,7 @@ const Page = () => {
     <PageView title="Home" className="gap-4">
       <Section>
         <Heading>Hello world!</Heading>
+        <Text>Text</Text>
         <Button onPress={toggleColorMode} className="mt-2">
           <ButtonText>Toggle color mode</ButtonText>
         </Button>
